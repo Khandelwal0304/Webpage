ee5139c48dbecd5e52087e6ebfda48c5d4c0557a
Khandelwal0304 <khandelwaljenendra96600@gmail.com>
2024-07-27 06:13:33 +0530

    Update

---
+++ script.js
@@ -0,0 +1,9 @@
+
+/* Additional JavaScript */
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+
+menuButton.addEventListener('click', () => {
+	menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+});
+```
